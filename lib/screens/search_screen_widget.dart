a40bfecb4edf3ca29a95a344a774c1736a96851b
BD911-Coder <68704206+BD911-Coder@users.noreply.github.com>
2022-09-02 15:51:24 +0300

    Minor changes

--- lib/screens/search_screen_widget.dart
+++ lib/screens/search_screen_widget.dart
@@ -3,6 +3,8 @@ import 'package:booking_app/utils/widgets/custom_container_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
+import '../utils/widgets/double_text_widget.dart';
+
 class SearchScreenWidget extends StatelessWidget {
   const SearchScreenWidget({Key? key}) : super(key: key);
 
@@ -36,6 +38,7 @@ class SearchScreenWidget extends StatelessWidget {
               ),
             ),
             child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
                   padding:
@@ -75,7 +78,7 @@ class SearchScreenWidget extends StatelessWidget {
                   ),
                   child: Center(
                     child: Text(
-                      'hotels',
+                      'Hotels',
                       style: Theme.of(context)
                           .primaryTextTheme
                           .button
@@ -115,6 +118,103 @@ class SearchScreenWidget extends StatelessWidget {
                 style: Theme.of(context).primaryTextTheme.headline5,
               ),
             ),
+          ),
+          const Gap(40),
+          const DoubleTextWidget(
+            widgetTitle: 'Upcoming Flights',
+            widgetSubtitle: 'View All',
+          ),
+          const Gap(15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: AppLayout.getHeight(400),
+                width: size.width * 0.42,
+                padding: EdgeInsets.symmetric(
+                    horizontal: AppLayout.getHeight(15),
+                    vertical: AppLayout.getWidth(0.1)),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(20)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Theme.of(context).colorScheme.onBackground,
+                      blurRadius: 10,
+                      spreadRadius: 1,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: AppLayout.getHeight(190),
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(AppLayout.getHeight(12)),
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/sits.png'),
+                        ),
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(12)),
+                    Text(
+                      '%20 discount on the early booking of this flight. Don\'t miss out this chance.',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: size.width * 0.44,
+                        height: AppLayout.getHeight(176),
+                        padding: EdgeInsets.symmetric(
+                            vertical: AppLayout.getHeight(15),
+                            horizontal: AppLayout.getHeight(15)),
+                        decoration: BoxDecoration(
+                            color:
+                                Theme.of(context).colorScheme.primaryContainer,
+                            borderRadius: BorderRadius.circular(
+                              AppLayout.getHeight(18),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color:
+                                    Theme.of(context).colorScheme.onBackground,
+                                blurRadius: 10,
+                                spreadRadius: 1,
+                              ),
+                            ]),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Discount for survey!',
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                            Gap(
+                              AppLayout.getHeight(10),
+                            ),
+                            Text(
+                              'Complete the survey for exclusive offers!',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                            Text(
+                              'Let\'s start',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
