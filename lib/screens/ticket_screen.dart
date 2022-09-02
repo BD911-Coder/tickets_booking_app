01512f0dd20801896c56baed35046e06b081de5d
BD911-Coder <68704206+BD911-Coder@users.noreply.github.com>
2022-09-02 19:31:01 +0300

    Ticket Screen added

---
+++ lib/screens/ticket_screen.dart
@@ -0,0 +1,18 @@
+import 'package:booking_app/utils/app_layout.dart';
+import 'package:flutter/material.dart';
+
+class TicketScreen extends StatelessWidget {
+  const TicketScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getSize;
+    return Scaffold(
+      body: Stack(
+        children: [ListView(
+          
+        )],
+      ),
+    );
+  }
+}
